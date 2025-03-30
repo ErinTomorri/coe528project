766893332dafd19a0d55127589cf5b9615b45567
votrihieu02 <162071701+votrihieu02@users.noreply.github.com>
2025-03-30 03:46:41 -0400

    Update Profile.java

--- coe528project/src/coe528project/Profile.java
+++ coe528project/src/coe528project/Profile.java
@@ -1,16 +1,28 @@
 
 package coe528project;
 
-public abstract class Profile {
+public class Profile {
     protected String username;
     protected String password;
     protected String role;
 
-    public void setPass(String pass) {
-        this.password = pass;
+    public Profile() {
+        this.username = "";
+        this.password = "";
+        rhis.role = "";
+    }
+    
+    public Profile(String username, String password) {
+        this.username = username;
+        this.password = password;
+        setRole();
     }
 
-    public String getPass() { 
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassord() { 
         return password;
     }
 
@@ -26,7 +38,18 @@ public abstract class Profile {
         this.username = username;
     }
 
-    public abstract void setRole();    
+    public void setRole() {
+        this.role = "User";    
+    }
+public boolean isAdmin() {
+        return "Admin".equals(role);
+    }
+    public boolean isCustomer() {
+        return "Customer".equals(role);
+    }
+    @Override
+    public String toString() {
+        return username + "," + password;
+    }
 }
-
 
